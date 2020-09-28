0274d5b07b5aeb4778706d67e93cc681dda44c20
ancestor <ancestor2003@126.com>
2020-09-28 13:10:35 +0800

    Update 复制粘贴功能.py

--- 脚本之家/操作系统运维类/复制粘贴功能.py
+++ 脚本之家/操作系统运维类/复制粘贴功能.py
@@ -1,1 +1,31 @@
-print('hello world')
+#功能：复制粘贴
+#url：https://www.jb51.net/article/14052.htm
+#-*- coding:utf-8 -*-
+import sys
+import os
+def readFile(filename):
+  file=open(filename,'r')
+  s=file.read().strip()
+  file.close()
+  return s
+def writeFile(filename,files)
+  content=[]
+  for f in files:
+    print('reading file')
+    s=readFile(f)
+    print('reading file completed')
+    contend.append(s)
+  print('writing file')
+  file=open(file,'w')
+  file.write('\n/*-----This is a seperating line.-----*/\n'.join(content))
+  file.close()
+  print('write file completed')
+filters=['.txt']
+fullpath=os.getcwd()
+print('opening directory',fullpath)
+sys.path.append(fullpath)
+files=[f for f in files if os.path.splitext(f)[1].lower() in filters]
+writeFile('beaunet_be_card.sql',files)
+
+
+  
