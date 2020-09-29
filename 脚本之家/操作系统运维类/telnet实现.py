7d2a2ae65d94d78a608dbc7468bc737989171775
ancestor <ancestor2003@126.com>
2020-09-29 14:01:15 +0800

    Update telnet实现.py

--- 脚本之家/操作系统运维类/telnet实现.py
+++ 脚本之家/操作系统运维类/telnet实现.py
@@ -2,4 +2,15 @@
 def telnetdo(host=none,user=none,pass=none,command=none):
   import telnetlib
   import sys
-  
+  if not host:
+    try:
+      host=sys.argv[1]
+      user=sys.argv[2]
+      pass=sys.argv[3]
+      command=sys.argv[4]
+    except:
+      print('usage:telnetdo.py host user pass command')
+      return
+   
+  msg.append(tn.expect(['login:'],5))
+  tn.write(user+'\n')
